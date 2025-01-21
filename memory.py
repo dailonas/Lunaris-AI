477cfb925f90f66a5762aba3c326dba81eb04130
daimyo <tanguydelone95@gmail.com>
2025-01-21 06:18:53 +0000

    memory update

--- memory.py
+++ memory.py
@@ -1,10 +1,14 @@
-import config
-import discord
+import config # Importation de la config
+import discord # Importation de la bibliotheque discordpy
 
-from groq import Groq
-client = Groq(api_key=config.groq_api_key)
+from groq import Groq # Importation du module groq
+client = Groq(api_key=config.api_key) # Création du client pour l'interaction avec l'api groq
 def generate_groq_response(prompt): # Gestion de la generation des reponses via un modèle d'ia
     try:
+        # Concaténer tous les éléments de la liste (l'historique de la conversation) en une seule chaîne
+        prompt = " ".join(prompt)
+
+        # Utiliser l'API Groq pour générer une réponse à partir du prompt
         chat_completion = client.chat.completions.create(
             messages=[
                 {
@@ -16,15 +20,17 @@ def generate_groq_response(prompt): # Gestion de la generation des reponses via 
                     "content": prompt,
                 }
             ],
-            model="llama-3.2-11b-vision-preview"  #"llama3-8b-8192", 
+            model="llama-3.2-11b-vision-preview"  # le modèl de langue utilisé 
         )
         
+        # Retourner la réponse générée
         return chat_completion.choices[0].message.content
-    except Exception as e:
+    except Exception as e: # Retourner un message d'erreur si une exception se produit
         return f"Une erreur c'est produite: {e}"
 
+# Classe pour gérer les conversations
 class memory:
-    def __init__(self, max_history=10): # Initialise le gestionnaire de conversations.
+    def __init__(self, max_history=5): # Initialise le gestionnaire de conversations.
         # max_history: Le nombre maximum de messages à conserver dans l'historique.
         
         self.conversations = {}
@@ -43,8 +49,8 @@ class memory:
 
         # Mettre à jour l'historique de la conversation avec la réponse générée
         self.conversations[user_id].append(reponse) # Ajouter la réponse à l'historique
-        self.conversations[user_id] = self.conversations[user_id][-self.max_history:] # Limiter l'historique
+        self.conversations[user_id] = self.conversations[user_id][-self.max_history:] # Limiter l'historique à la taille maximale
         return reponse
 
     def get_history(self, user_id): # Récupère l'historique de la conversation pour un utilisateur donné.
-        return self.conversations.get(user_id, []) # Une liste des messages de l'historique.
+        return self.conversations.get(user_id, []) # Retourner une liste vide si aucune conversation n'est disponible
